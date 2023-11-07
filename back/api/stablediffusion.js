976de1d7fb2852533d1903dbf11da89a4746e7dc
Valentin <valentin.renaudin@my-digital-school.org>
2023-11-07 14:56:35 +0100

    [ADD] Insertion des images en base quand elles sont générées

--- back/api/stablediffusion.js
+++ back/api/stablediffusion.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
-const dotenv = require('dotenv');
-dotenv.config({ path: '.env.local' });
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env.local" });
+const db = require("../../services/db");
 
 let stableDiffusionUrl = "https://clipdrop-api.co/text-to-image/v1";
 
@@ -31,8 +32,23 @@ async function generateImage(prompt) {
 
     fs.writeFileSync(outputDir + "/" + outputName, Buffer.from(buffer));
     console.log("Saved output to " + outputDir + "/" + outputName);
+
+    //INSERTION DE L'IMAGE EN BASE
+    insertQuery = "INSERT INTO images (url) VALUES (?)";
+
+    db.query(insertQuery, [outputName], (err, result) => {
+      if (err) {
+        console.error("Erreur lors de l'insertion de l'image : " + err);
+        res.status(500).json({ error: "Erreur serveur" });
+        return;
+      }
+
+      const imageId = result.insertId; // L'ID de l'image nouvellement créée
+
+     console.log("Image créée avec succès");
+    });
   } catch (error) {
-    console.error("Error generating or saving image:", error);
+    console.error("Impossible de sauvegarder l'image:", error);
   }
 }
 
